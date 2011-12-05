24413cbbef0748f51afc16a6606db7cac9e27b68
sinatraluigi@gmail.com <sinatraluigi@gmail.com@09d2b7ec-348a-4ed8-63d7-6edb3dd18e97>
2011-12-05 17:10:58 +0000

    Creata la prima parte del lexer di Lua Creato il file .CUP che però è di prova. Creato un main di prova all'interno di Program.java per provare il lexer con un file di prova chiamato prova.txt

--- src/s2sCompiler/Program.java
+++ src/s2sCompiler/Program.java
@@ -1,9 +1,27 @@
 package s2sCompiler;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import luaGrammar.*;
 public class Program
 {
-	public static void main()
+	public static void main(String args[])
 	{
 		System.out.println("source-to-source compiler.");
+		LuaGrammarLex lexer;
+		try {
+			lexer = new LuaGrammarLex(new FileReader("Prova.txt"));
+			LuaGrammarCup parser = new LuaGrammarCup(lexer);
+			parser.parse();
+		} catch (FileNotFoundException e1) {
+			System.out.println("File not found");
+			e1.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Eccezione");
+			e.printStackTrace();
+		}
 	}
 }
