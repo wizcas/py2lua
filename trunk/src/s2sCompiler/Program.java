a44f95cb31d195c695d0898423cf3683d4385487
sinatraluigi@gmail.com <sinatraluigi@gmail.com@09d2b7ec-348a-4ed8-63d7-6edb3dd18e97>
2011-12-16 15:12:05 +0000

    Modificato una parte di LuaGrammar.lex Aggiunte le stringhe e i numeri a Python.lex e Python.cup

--- trunk/src/s2sCompiler/Program.java
+++ trunk/src/s2sCompiler/Program.java
@@ -4,15 +4,31 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 import luaGrammar.*;
+import pyGrammar.*;
 public class Program
 {
 	public static void main(String args[])
 	{
 		System.out.println("source-to-source compiler.");
-		LuaGrammarLex lexer;
+		/*LuaGrammarLex luaLexer;
 		try {
-			lexer = new LuaGrammarLex(new FileReader("Prova.txt"));
-			LuaGrammarCup parser = new LuaGrammarCup(lexer);
+			luaLexer = new LuaGrammarLex(new FileReader("Prova.txt"));
+			LuaGrammarCup parser = new LuaGrammarCup(luaLexer);
+			parser.parse();
+		} catch (FileNotFoundException e1) {
+			System.out.println("File not found");
+			e1.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Eccezione");
+			e.printStackTrace();
+		}*/
+		
+		PythonLex lexer;
+		try {
+			lexer = new PythonLex(new FileReader("python.py"));
+			PythonCup parser = new PythonCup(lexer);
 			parser.parse();
 		} catch (FileNotFoundException e1) {
 			System.out.println("File not found");
