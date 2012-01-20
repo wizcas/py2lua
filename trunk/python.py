a18e9749a725c96efd9a069f00d769df07ad11e9
sinatraluigi@gmail.com <sinatraluigi@gmail.com@09d2b7ec-348a-4ed8-63d7-6edb3dd18e97>
2012-01-20 09:33:14 +0000

    Sistemata la traduzione di augassign di python in LUA che non accetta *=,+=, etc. Tradotto il for. Tradotta la funcdef con il parametro *args, non con **kargs

--- trunk/python.py
+++ trunk/python.py
@@ -1,8 +1,28 @@
-x=0
-
-while a<b:
-	while c<b:
-		print "fine while innestato"
-		print "c"
-		print "aaaa"
-		print "bbbbb"
+s=0;
+pippo=40;
+while s<100:
+	pippo=pippo/2;
+	ciccio=3
+	s=s + ciccio;
+	if (s>8) & (pippo<10):
+		break
+	elif (s<8) | (pippo<10):
+		print "Ciao"
+	print s
+a = 0
+while a < 15:
+	a = a + 1
+	if (a > 5)&(a<=10):
+		print a," > ",5
+	elif a < 5:
+		print a," < ",5
+	elif a > 10:
+		print "Troppo grande for me"
+		break
+	else:
+		b=a**2
+		c=a
+		c*=2
+		print "Hello 5 il quadrato e"
+		print "Hello 5 il quadrato e' ", b, "il doppio e' ", c
+print "Salutamu"
