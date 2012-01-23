78e6a718d60fd389b41f9f75240e0f8b3823d051
sinatraluigi@gmail.com <sinatraluigi@gmail.com@09d2b7ec-348a-4ed8-63d7-6edb3dd18e97>
2012-01-23 15:52:56 +0000

    Inserita l'accettazione dei parametri di default in LUA nella funcdef

--- python.py
+++ python.py
@@ -1,14 +1,16 @@
-s=0;
-pippo=40;
-while s<100:
-	pippo=pippo/2;
-	ciccio=3
-	s=s + ciccio;
-	if (s>8) & (pippo<10):
-		break;
-	elif (s<8) | (pippo<10):
-		print "Ciao";
-	print s
+def funzione1():
+	c= "gigi la trottola"
+	print c
+
+def funzione2(a, b=1):
+	c= a+b;
+	print c
+
+def funzione3 (a=4,f=18,aaa):
+	c = a*f
+	print c
+	for k in aaa:
+		print k
 a = 0
 while a < 15:
 	a = a + 1
@@ -18,16 +20,15 @@ while a < 15:
 		print a," < ",5
 	elif a > 10:
 		print "Troppo grande for me"
-		break
+		print "funzione2 (passo un parametro): ",
+		funzione2(a) 
+		print "funzione2 (passo due parametri): ",
+		funzione2(a,4)
+		break;
 	else:
 		b=a**2
 		c=a
 		c*=2
-		print "Hello 5 il quadrato e'"
+		print "Hello 5 il quadrato e' ", b, "il doppio e' ", c
+		print "funzione1: ",funzione1()
 print "Salutamu"
-
-
-def f(a=1,b=2):
-	print a,b
-
-
