c7a283d42ce02092e9500b1f445f1f2bbfe290f5
sinatraluigi@gmail.com <sinatraluigi@gmail.com@09d2b7ec-348a-4ed8-63d7-6edb3dd18e97>
2012-02-13 15:14:08 +0000

    Correzione di piccole cose nella traduzione

--- branches/attrs/python.py
+++ branches/attrs/python.py
@@ -1,1 +1,43 @@
-a = 1,2,3
+while a<b:
+	while c<d:
+		while e<f:
+			print "ciao"
+		print "fine while innestato"
+	print "something else"
+print "fin"
+
+def funzione1():
+	c= "gigi la trottola"
+	print c
+
+def funzione2(a, b=1):
+	c= a+b;
+	print c	
+
+if a>0:
+	print "a > 0"
+elif a == 0:
+	print 'a == 0'
+else:
+	print 'a < 0'
+
+a = 0
+while a < 15:
+	a = a + 1
+	if (a > 5)&(a<=10):
+		print a," > ",5
+	elif a < 5:
+		print a," < ",5
+	elif a > 10:
+		print "Troppo grande for me"
+		print "funzione2 (passo un parametro): ",
+		funzione2(a) 
+		print "funzione2 (passo due parametri): ",
+		funzione2(a,4)
+		break;
+	else:
+		a/=2
+		c=a-4*80
+		c*=18
+		print "prova"
+
